845a7bfb1e7f5cc12d788eb460af87999bb9d87e
Steve Godin <steve.god@gmail.com>
2017-04-28 09:12:40 -0700

    Add Messages#update

--- routes/messages.js
+++ routes/messages.js
@@ -16,6 +16,17 @@ router.post('/', function(req, res, next) {
     .then(() => res.status(201).end())
 });
 
+router.patch('/:id', function(req, res, next) {
+  const {id} = req.params;
+  const {content} = req.body;
+
+  Message
+    .findById(id)
+    .then(message => message.update({content}))
+    .then(() => res.status(200).end())
+    .catch(() => res.status(400).end())
+});
+
 router.delete('/:id', function(req, res, next) {
   const {id} = req.params;
 
