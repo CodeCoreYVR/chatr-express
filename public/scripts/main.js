2dd5983feb7f9e5cb5cfea29e0b7eb5512db1738
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-05-12 17:05:59 -0700

    setup chatr project

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,8 @@
+// Clone chatr project from https://github.com/CodeCoreYVR/chatr-express.git
+// cd into directory
+// nvm install 12.18.4
+// and/or nvm use 12.18.4
+// npm i fsevents
+// npm run db:setup
+
 // Write chatr code here!
